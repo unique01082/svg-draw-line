9615f62d515f7fe665164c9918d63219d85129fa
Bao Le <>
2024-08-25 18:25:41 +0700

    feat: updated version

--- src/Icon.jsx
+++ src/Icon.jsx
@@ -11,31 +11,38 @@ function revisedRandId() {
 function Icon({ type, style, ...rest }) {
   const { current: id } = useRef(revisedRandId());
   const ref = useRef();
-  const animate = () => {
-    console.log(1);
-    anime({
-      targets: ref.current.querySelectorAll(`#${id} path`),
-      strokeDashoffset: [anime.setDashoffset, 0],
-      fillOpacity: [0, 1],
-      easing: "easeInOutSine",
-      duration: 1000,
-      delay: (el, i) => i * 200,
-      direction: "normal",
-    });
-  };
+  const animateRef = useRef();
   useEffect(() => {
     setTimeout(() => {
-      console.log("type :>> ", type);
-      const content = document.querySelector(`#${type}`)?.outerHTML;
-      console.log("content :>> ", content);
-      ref.current.insertAdjacentHTML("afterBegin", content.replace(type, id));
-
+      const source = document.querySelector(`#${type}`);
+      const clone = source.cloneNode(true);
+      console.log("clone :>> ", clone);
+      clone.id = id;
+      const els = clone.querySelectorAll("path");
+      els.forEach((el) => {
+        el.setAttribute("stroke", el.getAttribute("fill"));
+        // el.setAttribute("fill", "none");
+        el.setAttribute("fill", el.getAttribute("fill"));
+      });
+      ref.current.prepend(clone);
       console.log(
         "ref.current.querySelectorAll(`#${id} path`) :>> ",
         ref.current.querySelectorAll(`#${id} path`)
       );
 
-      animate();
+      const animate = anime({
+        targets: ref.current.querySelectorAll(`#${id} path`),
+        strokeDashoffset: [anime.setDashoffset, 0],
+        strokeWidth: [15, 10, 5, 0],
+        fillOpacity: { value: [0, 1], delay: 2400 },
+        // fill: ['#793ee6', '#c3de2c'],
+        easing: "easeInOutSine",
+        duration: 3000,
+        delay: (el, i) => i * 300,
+        direction: "normal",
+      });
+
+      animateRef.current = animate;
     }, 100);
   }, [id]);
 
@@ -45,7 +52,9 @@ function Icon({ type, style, ...rest }) {
       className="anticon"
       style={style}
       data-type={type}
-      onClick={animate}
+      onClick={() => {
+        animateRef.current.restart();
+      }}
     >
       <svg
         ref={ref}
@@ -56,7 +65,7 @@ function Icon({ type, style, ...rest }) {
         focusable="false"
         {...rest}
       >
-        <use xlinkHref={`#${id}`}></use>
+        <use xlinkHref={`#${id}`} />
       </svg>
     </span>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,50 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createFromIconfontCN } from "@ant-design/icons";
+import Icon from "./Icon.jsx";
+
+const icons = [
+  "apd-tu",
+  "apd-she",
+  "apd-hu",
+  "apd-gou",
+  "apd-ji",
+  "apd-niu",
+  "apd-long",
+  "apd-yang",
+  "apd-shu",
+  "apd-ma",
+  "apd-hou",
+  "apd-zhu",
+  "apd-baiyang",
+  "apd-jinniu",
+  "apd-mojie",
+  "apd-sheshou",
+  "apd-chunv",
+  "apd-shizi",
+  "apd-shuangyu",
+  "apd-shuiping",
+  "apd-juxie",
+  "apd-tiancheng",
+  "apd-shuangzi",
+  "apd-tianxie",
+];
 
 createFromIconfontCN({
   scriptUrl: `/icons/animated-icons.js`,
 });
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <div
+    style={{
+      display: "grid",
+      gridAutoRows: 128,
+      gridTemplateColumns: "repeat(2, 1fr)",
+      gap: 32,
+    }}
+  >
+    {icons.map((icon) => (
+      <Icon key={icon} type={icon} style={{ fontSize: 128 }} />
+    ))}
+  </div>
+);
